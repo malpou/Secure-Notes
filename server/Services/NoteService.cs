e0bbe506acd3a12192a33f304a53c947b2169cd8
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-08-07 20:45:49 +0200

    🐛 fix(NoteService.cs): fix namespace declaration and add missing using statements ✨ feat(NoteService.cs): add CreateAsync method to create a new note with encrypted content ✨ feat(NoteService.cs): add GetAllAsync method to retrieve all notes with optional decryption 🐛 fix(UserService.cs): fix namespace declaration and add missing using statements ✨ feat(UserService.cs): add CreateUser method to create a new user with a unique key ✨ feat(UserService.cs): add Login method to authenticate a user and generate a JWT token ✨ feat(UserService.cs): add GetUser method to retrieve a user by username

--- server/Services/NoteService.cs
+++ server/Services/NoteService.cs
@@ -1,5 +1,79 @@
-﻿namespace SecureNotes.Functions.Services;
+﻿using System.Text;
+using SecureNotes.Functions.Entities;
+using SecureNotes.Functions.Helpers;
+using SecureNotes.Functions.Requests;
+
+namespace SecureNotes.Functions.Services;
 
 public class NoteService
 {
+    private readonly CryptographyHelper _cryptoHelper;
+    private readonly TableStorageHelper<Note> _tableStorageHelper;
+
+    public NoteService(TableStorageHelper<Note> tableStorageHelper, CryptographyHelper cryptoHelper)
+    {
+        _tableStorageHelper = tableStorageHelper;
+        _cryptoHelper = cryptoHelper;
+    }
+
+    public async Task<Note> CreateAsync(NoteRequest newNote, string userId)
+    {
+        var contentBytes = Encoding.UTF8.GetBytes(newNote.Content);
+        var encryptedContent = await _cryptoHelper.EncryptData(contentBytes, userId);
+
+        var encryptedNote = new Note
+        {
+            RowKey = Guid.NewGuid().ToString(),
+            Title = newNote.Title,
+            Content = encryptedContent,
+            CreatedTime = DateTimeOffset.UtcNow,
+            PartitionKey = userId
+        };
+
+        await _tableStorageHelper.AddEntityAsync(encryptedNote);
+
+        return encryptedNote;
+    }
+    
+    public async Task<IEnumerable<Note>> GetAllAsync(string userId, int page, int pageSize)
+    {
+        var notes = (await _tableStorageHelper.GetAllEntitiesAsync())
+            .OrderByDescending(GetNoteComparisonDate)
+            .Skip((page - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var cleanedNotes = new List<Note>();
+        
+        if (!string.IsNullOrEmpty(userId))
+        {
+            var decryptedNotes = await Task.WhenAll(notes
+                .Where(note => note.PartitionKey == userId)
+                .Select(note => DecryptNoteAsync(note, userId)));
+
+            cleanedNotes.AddRange(decryptedNotes);
+        }
+
+        foreach (var note in notes.Where(note => note.PartitionKey != userId))
+        {
+            var contentWithoutKey = note.Content.Split(":")[1];
+            note.Content = contentWithoutKey;
+
+            cleanedNotes.Add(note);
+        }
+
+        return cleanedNotes.OrderByDescending(GetNoteComparisonDate);
+    }
+
+    private async Task<Note> DecryptNoteAsync(Note note, string userId)
+    {
+        var decryptedContent = await _cryptoHelper.DecryptData(note.Content, userId);
+        note.Content = decryptedContent;
+        return note;
+    }
+    
+    private static DateTimeOffset GetNoteComparisonDate(Note note)
+    {
+        return note.LastUpdatedTime != DateTimeOffset.MinValue ? note.LastUpdatedTime : note.CreatedTime;
+    }
 }
--- server/Services/UserService.cs
+++ server/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using SecureNotes.Functions.Entities;
+﻿using Azure.Security.KeyVault.Keys;
+using SecureNotes.Functions.Entities;
 using SecureNotes.Functions.Helpers;
 
 namespace SecureNotes.Functions.Services;
@@ -6,12 +7,14 @@ namespace SecureNotes.Functions.Services;
 public class UserService
 {
     private readonly JwtHelper _jwtHelper;
+    private readonly KeyHelper _keyHelper;
     private readonly TableStorageHelper<User> _tableStorageHelper;
 
-    public UserService(TableStorageHelper<User> tableStorageHelper, JwtHelper jwtHelper)
+    public UserService(TableStorageHelper<User> tableStorageHelper, JwtHelper jwtHelper, KeyHelper keyHelper)
     {
         _tableStorageHelper = tableStorageHelper;
         _jwtHelper = jwtHelper;
+        _keyHelper = keyHelper;
     }
 
     public async Task<string> CreateUser(string username, string password)
@@ -29,6 +32,9 @@ public class UserService
             CreatedTime = DateTimeOffset.UtcNow
         };
 
+        var keyName = "master-key-" + user.RowKey;
+        await _keyHelper.CreateKeyAsync(keyName, KeyType.RsaHsm);
+
         await _tableStorageHelper.AddEntityAsync(user);
 
         return await Login(username, password);
@@ -36,7 +42,7 @@ public class UserService
 
     public async Task<string> Login(string username, string password)
     {
-        var user = await _tableStorageHelper.GetEntityByPartitionKeyAsync(username);
+        var user = await GetUser(username);
 
         if (user == null)
             throw new Exception("User does not exist");
@@ -48,6 +54,11 @@ public class UserService
 
         await _tableStorageHelper.UpdateEntityAsync(user, user.ETag);
 
-        return _jwtHelper.Generate(user.RowKey, user.PartitionKey);
+        return _jwtHelper.Generate(user.PartitionKey);
+    }
+
+    public async Task<User?> GetUser(string username)
+    {
+        return await _tableStorageHelper.GetEntityByPartitionKeyAsync(username);
     }
 }
