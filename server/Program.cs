c06a11e73194f04a83aeadca858fe7544bafa8f9
Malthe Poulsen <malthe@grundtvigsvej.dk>
2023-08-07 16:53:53 +0200

    🔧 chore(Program.cs): refactor Program.cs to use dependency injection for improved modularity and testability

--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SecureNotes.Functions.Entities;
+using SecureNotes.Functions.Helpers;
+using SecureNotes.Functions.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s =>
+        {
+            var storageConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            var keyVaultUrl = Environment.GetEnvironmentVariable("KeyVaultUrl");
+
+
+            s.AddSingleton(_ => new TableStorageHelper<User>(storageConnectionString!, "users"));
+            s.AddSingleton(_ => new TableStorageHelper<Note>(storageConnectionString!, "notes"));
+
+            s.AddSingleton(_ => new SecretHelper(keyVaultUrl!));
+            s.AddSingleton(_ => new JwtHelper(
+                s.BuildServiceProvider().GetRequiredService<SecretHelper>().GetSecretAsync("jwt-secret").Result
+            ));
+
+            s.AddSingleton<UserService>();
+            s.AddSingleton<NoteService>();
+        }
+    )
     .Build();
 
 host.Run();
